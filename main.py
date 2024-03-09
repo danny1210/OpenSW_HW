8c07512ff192e6d60f1505ab25743d937b56c749
danny1210 <dldndgml1210@naver.com>
2024-03-09 12:33:59 +0900

    이웅희 2020112008

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-print("이름과 학번을 입력하세요.")
+print("이름과 학번, 학과를 입력하세요.")
 print("이름 : " ,end='');name = input()
 print("학번 : " ,end='');studentNumber = input()
+print("학과 : ",end='');majorIn = input()
 
 print("이름은 "+ name)
-print("학번은 "+ studentNumber+"입니다.")
+print("학번은 "+ studentNumber)
+print("학과는 "+ majorIn+"입니다.")
